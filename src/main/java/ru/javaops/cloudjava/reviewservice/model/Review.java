2c419f92d08b46de64cd4847f1e6f9efd9ea52e7
Andrey Falkovskiy <avf-91@yandex.ru>
2025-11-04 17:29:56 +0300

    ДЗ-1: создание JPA-модели

---
+++ src/main/java/ru/javaops/cloudjava/reviewservice/model/Review.java
@@ -0,0 +1,47 @@
+package ru.javaops.cloudjava.reviewservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Table(name = "reviews")
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "menu_id", nullable = false)
+    private Long menuId;
+    @Column(name = "created_by", nullable = false)
+    private String createdBy;
+    @Column(name = "comment")
+    private String comment;
+    @Column(name = "rate", nullable = false)
+    private int rate;
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        return getId() != null && getId().equals(((Review) o).getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Hibernate.getClass(this).hashCode();
+    }
+}
